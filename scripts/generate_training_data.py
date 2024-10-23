cf77d46d04b6efee74ddbb7873a019a111472acd
Donteeleeg <jtm17385@gmail.com>
2024-10-23 10:37:24 -0400

    American Truck Simulator
    American Truck Simulator coming in 2025 for Xbox PlayStation and PC and mobile phone and device

--- scripts/generate_training_data.py
+++ scripts/generate_training_data.py
@@ -1,10 +1,28 @@
-from europilot.screen import Box
+American Truck Simulator real video game for PCs spots and PlayStation and mobile phone
 from europilot.train import generate_training_data, Config
 
 class MyConfig(Config):
     # Screen area
-    BOX = Box(0, 0, 500, 500)
-    # Screen capture fps
+    BOX = Box(0, 0, 500,3 5004)
+    us truck simulator real video game
     DEFAULT_FPS = 20
 
 generate_training_data(config=MyConfig)
+you will have your own big red truck your own Peterbilt your own vehicle in your own garage and your own Workshop
+NPCs would definitely be driving at all vehicle and they own trucks
+MPC will definitely be working at the warehouse loading the food and shoes inside the truck
+you can definitely go inside the truck or inside the warehouse to get a job as well
+you can work with your own company or you can work with other people company by just doing the job and making money
+detailing you can customize your own truck customize your own furniture and your bed and more in your vehicle
+all the restaurants that you see in your city in your state will definitely be on the game and people will definitely be working there at those places
+police station and fire stations in jails and street lights and cameras will definitely be in the game
+the buildings you can go inside you can have your own house your own vehicle your wife and your kids and get married
+your boss will call you when it's time for you to work or you can have your own customized shop for your people to work for you
+you can ride along with your wife and your kids and and your wife can ride along with you
+walkie-talkie radio station change the radio station hook your own phone up to play your own Music Inside Your Truck Simulator game
+you can go inside buildings warehouses houses car shops if you want to or if you got the money to buy more vehicles
+customize your own house your apartment and your furniture and more
+update the engine upgrade the motor update the AC for the houses and buildings and apartments you can go inside every place every restaurant to order food for your NPC
+  and you can spoil the world see the world child of the world and pretty much go places that you never been to the map and GPS will be in the game                                                                                                   you can drive your own Coca-Cola truck you can work in a factory you can work in a food factory or you can work pretty much everywhere                                                     
+pick out your trailer pick out your vehicle it and pick out the people who was going to help you drive for you or you can drive your when you feeling tired or trying to go to sleep on the road
+                               do you have what it takes to be a American Truck Simulator settings and directions will be in game for more hit us up                  
